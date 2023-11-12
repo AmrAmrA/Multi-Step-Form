c62ce3e3b599142858d94217b77d1c56768add8f
AmrAmrA <amrelfattah.dev@gmail.com>
2023-11-12 19:37:43 +0100

    try to understand union types in Typescript

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = 5;
+var user = new Student("Mikhouri", "Mikhouri.", "User");
 document.body.textContent = greeter(user);
+var world = false;
+var myWindow = "open";
--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
-function greeter (person: string) {
-    return "Hello, " + person;
+class Student {
+    fullName: string;
+    constructor(
+      public firstName: string,
+      public middleInitial: string,
+      public lastName: string
+    ) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
   }
    
-  let user = 5;
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+   
+  function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+  }
+   
+  let user = new Student("Mikhouri", "Mikhouri.", "User");
    
   document.body.textContent = greeter(user);
-  
+
+  let world = false; 
+
+  type WindowStates = "open" | "closed" | "minimized";
+
+  const myWindow : WindowStates = "open"
