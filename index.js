8d2208f6d9551e4b8794cfaff6a9bb490547b5f3
AmrAmrA <amrelfattah.dev@gmail.com>
2023-11-14 01:47:57 +0100

    testing optional properties

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ var VirtualPoint = /** @class */ (function () {
 function logPoint(p) {
     console.log("".concat(p.x, ", ").concat(p.y));
 }
-var newVPoint = { y: 1, x: 55, };
+var newVPoint = { y: 1, x: 55 };
 logPoint(newVPoint);
 var arr;
 var newArray = ["hello", "World", "Everybody"];
+var newMyObject = { first: "Amir" };
+console.log(newMyObject.first, newMyObject.lastName);
--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
 class VirtualPoint {
-    x: number;
-    y: number;
-   
-    constructor(x: number, y: number) {
-      this.x = x;
-      this.y = y;
-    }
-  }
-  function logPoint(p: VirtualPoint) {
-    console.log(`${p.x}, ${p.y}`);
-  }
-   
-   
-  const newVPoint : VirtualPoint = {y : 1, x : 55,}
-  logPoint(newVPoint);
+  x: number;
+  y: number;
 
- let arr : string[]; 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+function logPoint(p: VirtualPoint) {
+  console.log(`${p.x}, ${p.y}`);
+}
 
-  const newArray : typeof arr = ["hello", "World", "Everybody"]
+const newVPoint: VirtualPoint = { y: 1, x: 55 };
+logPoint(newVPoint);
+
+let arr: string[];
+
+const newArray: typeof arr = ["hello", "World", "Everybody"]; 
+
+interface myObject {first : string, lastName ? : string}
+
+const newMyObject: myObject = {first : "Amir"}
+
+console.log(newMyObject.first, newMyObject.lastName);
